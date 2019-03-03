5f7ac3973e29302ff725c2b4c32c9afeb721f827
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-03 15:55:15 +0100

    rewriting using OOP

--- src/test/CaesarCypherTest.java
+++ src/test/CaesarCypherTest.java
@@ -4,18 +4,21 @@ import static org.junit.Assert.assertEquals;
 
 public class CaesarCypherTest {
 
-    CaesarCipher cc = new CaesarCipher();
-
     @Test
     public void testEncrypt() {
-        assertEquals("Ymj rtwj N qjfws fgtzy ujtuqj, ymj rtwj N qnpj rd itl.", cc.encrypt("The more I learn about people, the more I like my dog.", 5));
-        assertEquals("Zcuypc md dyjqc ilmujcbec; gr gq kmpc bylecpmsq rfyl gelmpylac.", cc.encrypt("Beware of false knowledge; it is more dangerous than ignorance.", 24));
+        CaesarCipher cc1 = new CaesarCipher(5);
+        assertEquals("Ymj rtwj N qjfws fgtzy ujtuqj, ymj rtwj N qnpj rd itl.", cc1.encrypt("The more I learn about people, the more I like my dog."));
+        CaesarCipher cc2 = new CaesarCipher(24);
+        assertEquals("Zcuypc md dyjqc ilmujcbec; gr gq kmpc bylecpmsq rfyl gelmpylac.", cc2.encrypt("Beware of false knowledge; it is more dangerous than ignorance."));
     }
 
     @Test
     public void testEncryptTwoKeys() {
-        assertEquals("Zbzbzbz", cc.encryptTwoKeys("Aaaaaaa", 25, 1));
-        assertEquals("Aeaeaea", cc.encryptTwoKeys("Ccccccc", 24, 2));
-        assertEquals("Run like wild to beat the wind", cc.encryptTwoKeys("Top ncmy qkff vi vguv vbg ycpx", 24, 6));
+        CaesarCipher cc1 = new CaesarCipher(25, 1);
+        assertEquals("Zbzbzbz", cc1.encryptTwoKeys("Aaaaaaa"));
+        CaesarCipher cc2 = new CaesarCipher(24, 2);
+        assertEquals("Aeaeaea", cc2.encryptTwoKeys("Ccccccc"));
+        CaesarCipher cc3 = new CaesarCipher(24, 6);
+        assertEquals("Run like wild to beat the wind", cc3.encryptTwoKeys("Top ncmy qkff vi vguv vbg ycpx"));
     }
 }
