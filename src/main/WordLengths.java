6a78589639d7f06e7f2314ff8467b17fbc7993f2
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-03 12:38:59 +0100

    implementing countWordLengths() method

---
+++ src/main/WordLengths.java
@@ -0,0 +1,26 @@
+import edu.duke.FileResource;
+
+class WordLengths {
+
+    static int[] countWordLengths(FileResource resource, int[] counts) {
+
+        int[] newCount = new int[counts.length + 1];
+
+        for (String word : resource.words()) {
+            int length;
+            if (!Character.isLetter(word.charAt(0)) & !Character.isLetter(word.charAt(word.length() - 1)))
+                length = word.length() - 2;
+            else if (!Character.isLetter(word.charAt(0)) || !Character.isLetter(word.charAt(word.length() - 1)))
+                length = word.length() - 1;
+            else
+                length = word.length();
+
+            if (length < newCount.length) {
+                newCount[length]++;
+            } else {
+                newCount[newCount.length - 1]++;
+            }
+        }
+        return newCount;
+    }
+}
---
+++ src/test/WordLengthsTest.java
@@ -0,0 +1,22 @@
+import edu.duke.FileResource;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WordLengthsTest {
+    private FileResource resource = new FileResource("resources/smallHamlet.txt");
+    private int[] counter = new int[11];
+
+    @Test
+    public void testCountWordLengths() {
+        counter =  WordLengths.countWordLengths(resource, counter);
+        assertEquals(2, counter[2]);
+        assertEquals(3, counter[3]);
+        assertEquals(2, counter[4]);
+        assertEquals(1, counter[5]);
+        assertEquals(1, counter[6]);
+        assertEquals(1, counter[7]);
+        assertEquals(2, counter[8]);
+        assertEquals(1, counter[11]);
+    }
+}
