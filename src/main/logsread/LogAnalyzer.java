b14f7881d239476b308a061af6acf44e21e89cfa
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-10 10:04:46 +0100

    counting unique IPs

--- src/main/logsread/LogAnalyzer.java
+++ src/main/logsread/LogAnalyzer.java
@@ -8,26 +8,15 @@ import java.util.ArrayList;
  * Allows to read server logs and to store data in java objects.
  *
  * @author (Yaroslav Rybiak)
- * @version (09.03.2019)
+ * @version (10.03.2019)
  */
 public class LogAnalyzer {
     private ArrayList<LogEntry> records;
 
-    public LogAnalyzer() {
-        records = new ArrayList<>();
-    }
-
-    public LogAnalyzer(String filename) {
+    LogAnalyzer(String filename) {
         FileResource fr = new FileResource(filename);
         records = new ArrayList<>();
-        for (String line: fr.lines()) {
-            records.add(WebLogParser.parseEntry(line));
-        }
-    }
-
-    public void readFile(String filename) {
-        FileResource fr = new FileResource(filename);
-        for (String line: fr.lines()) {
+        for (String line : fr.lines()) {
             records.add(WebLogParser.parseEntry(line));
         }
     }
@@ -38,9 +27,34 @@ public class LogAnalyzer {
         }
     }
 
-    public LogEntry getLine(int number) {
+    LogEntry getLine(int number) {
         return records.get(number);
     }
 
+    int countUniqueIPs() {
+        ArrayList<String> unique = new ArrayList<>();
+        String ip;
+        for (LogEntry record : records) {
+            ip = record.getIpAddress();
+            if (!unique.contains(ip)) {
+                unique.add(ip);
+            }
+        }
+        return unique.size();
+    }
 
+    int uniqueIPVisitsOnDay(String day) {
+        //date format "MMM DD"
+        ArrayList<String> unique = new ArrayList<>();
+        String ip;
+        String date;
+        for (LogEntry record : records) {
+            ip = record.getIpAddress();
+            date = record.getAccessTime().toString();
+            if (date.contains(day) && !unique.contains(ip)) {
+                unique.add(ip);
+            }
+        }
+        return unique.size();
+    }
 }
--- src/test/logsread/LogAnalyzerTest.java
+++ src/test/logsread/LogAnalyzerTest.java
@@ -1,27 +1,40 @@
 package logsread;
-import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for LogAnalyzer class.
  *
  * @author (Yaroslav Rybiak)
- * @version (09.03.2019)
+ * @version (10.03.2019)
  */
 public class LogAnalyzerTest {
 
+    private LogAnalyzer la = new LogAnalyzer("resources/logsread/weblog1.log");
+
     @Test
     public void testLogAnalyzer() {
-        LogAnalyzer la = new LogAnalyzer("resources/logsread/weblog1.log");
-
-        LogEntry le1 =  la.getLine(0);
+        LogEntry le1 = la.getLine(0);
         assertEquals("177.4.40.87", le1.getIpAddress());
         assertEquals(200, le1.getStatusCode());
         assertEquals(1127, le1.getBytesReturned());
 
-        LogEntry le2 =  la.getLine(1);
+        LogEntry le2 = la.getLine(1);
         assertEquals("177.4.40.87", le2.getIpAddress());
         assertEquals(200, le2.getStatusCode());
         assertEquals(3550, le2.getBytesReturned());
+    }
+
+    @Test
+    public void testCountUniqueIPs() {
+        assertEquals(75, la.countUniqueIPs());
+    }
+
+    @Test
+    public void testUniqueIPVisitsOnDay() {
+        assertEquals(7, la.uniqueIPVisitsOnDay("Mar 15"));
+        assertEquals(5, la.uniqueIPVisitsOnDay("Mar 26"));
     }
 }
