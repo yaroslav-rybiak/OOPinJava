0c145f9a66b5a7ea22752705d8c9e3feed2dac2f
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-11 21:36:47 +0100

    CaesarCypher as provided

---
+++ src/main/vigenere/CaesarCipher.java
@@ -0,0 +1,49 @@
+package vigenere;
+
+class CaesarCipher {
+
+    private int key;
+
+    CaesarCipher(int key) {
+        this.key = key;
+    }
+
+    String encrypt(String input) {
+        //Make a StringBuilder with message (encrypted)
+        StringBuilder encrypted = new StringBuilder(input);
+        //Write down the alphabet
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //Compute the shifted alphabet
+        String shiftedAlphabet = alphabet.substring(key) +
+                alphabet.substring(0, key);
+        //Count from 0 to < length of encrypted, (call it i)
+        for (int i = 0; i < encrypted.length(); i++) {
+            //Look at the ith character of encrypted (call it currChar)
+            boolean isUp = false;
+            char currChar = Character.toUpperCase(encrypted.charAt(i));
+            if (currChar == encrypted.charAt(i)) isUp = true;
+            //Find the index of currChar in the alphabet (call it idx)
+            int idx = alphabet.indexOf(currChar);
+            //If currChar is in the alphabet
+            if (idx != -1) {
+                //Get the idxth character of shiftedAlphabet (newChar)
+                char newChar = shiftedAlphabet.charAt(idx);
+                //Replace the ith character of encrypted with newChar
+                if (isUp) {
+                    encrypted.setCharAt(i, newChar);
+                } else {
+                    encrypted.setCharAt(i, Character.toLowerCase(newChar));
+                }
+            }
+            //Otherwise: do nothing
+        }
+        //Your answer is the String inside of encrypted
+        return encrypted.toString();
+    }
+
+    String decrypt(String input) {
+        key = 26 - key;
+        return encrypt(input);
+    }
+}
+
---
+++ src/test/vigenere/CaesarCipherTest.java
@@ -0,0 +1,18 @@
+package vigenere;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CaesarCipherTest {
+
+
+    @Test
+    public void testEncrypt() {
+        CaesarCipher cs = new CaesarCipher(17);
+        String encrypted = cs.encrypt("FREE CAKE IN THE CONFERENCE ROOM!");
+        String decrypted = cs.decrypt("WIVV TRBV ZE KYV TFEWVIVETV IFFD!");
+        assertEquals("WIVV TRBV ZE KYV TFEWVIVETV IFFD!", encrypted);
+        assertEquals("FREE CAKE IN THE CONFERENCE ROOM!", decrypted);
+    }
+}
