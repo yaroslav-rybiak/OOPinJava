87fec911ae4bd04fde0e606b7245c13d2db4c864
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-14 19:35:16 +0100

    implementing vigenere breaker for any language

--- src/main/vigenere/VigenereBreaker.java
+++ src/main/vigenere/VigenereBreaker.java
@@ -2,9 +2,28 @@ package vigenere;
 
 import edu.duke.FileResource;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class VigenereBreaker {
+
+    private HashMap<String, HashSet<String>> languages;
+
+    //constructor
+    public VigenereBreaker() {
+        languages = new HashMap<>();
+        languages.put("Danish", readDictionary("resources/vigenere/dictionaries/Danish"));
+        languages.put("Dutch", readDictionary("resources/vigenere/dictionaries/Dutch"));
+        languages.put("English", readDictionary("resources/vigenere/dictionaries/English"));
+        languages.put("French", readDictionary("resources/vigenere/dictionaries/French"));
+        languages.put("German", readDictionary("resources/vigenere/dictionaries/German"));
+        languages.put("Italian", readDictionary("resources/vigenere/dictionaries/Italian"));
+        languages.put("Portuguese", readDictionary("resources/vigenere/dictionaries/Portuguese"));
+        languages.put("Spanish", readDictionary("resources/vigenere/dictionaries/Spanish"));
+    }
+
     public String sliceString(String message, int start, int step) {
         StringBuilder sb = new StringBuilder();
         for (int i = start; i < message.length(); i += step) {
@@ -30,17 +49,18 @@ public class VigenereBreaker {
     public HashSet<String> readDictionary(String pathToDictionary) {
         FileResource fr = new FileResource(pathToDictionary);
         HashSet<String> words = new HashSet<>();
-        for(String line: fr.lines()) {
+        for (String line : fr.lines()) {
             words.add(line.toLowerCase());
         }
         return words;
     }
 
     public int countWords(String message, HashSet<String> dictionary) {
-        String[] words = message.split("\\W");
+        String[] words = message.split("\\W+");
         int amount = 0;
-        for (String word: words) {
-            if(dictionary.contains(word)) amount++;
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (dictionary.contains(word)) amount++;
         }
         return amount;
     }
@@ -54,12 +74,35 @@ public class VigenereBreaker {
             vc = new VigenereCipher(key);
             String decrypted = vc.decrypt(enrypted);
             int countWords = countWords(decrypted, dictionary);
-            if(countWords > max) {
+            if (countWords > max) {
                 max = countWords;
                 finalResult = decrypted;
             }
         }
         return finalResult;
+    }
+
+    public char mostCommonCharIn(HashSet<String> dictionary) {
+        HashMap<Character, Integer> chars = new HashMap<>();
+        for (String word : dictionary) {
+            for (char c : word.toCharArray()) {
+                if (!chars.containsKey(c)) {
+                    chars.put(c, 1);
+                } else {
+                    chars.put(c, chars.get(c) + 1);
+                }
+            }
+        }
+        int max = 0;
+        char result = '*';
+        for (Map.Entry<Character, Integer> e : chars.entrySet()) {
+            if (max < e.getValue()) {
+                max = e.getValue();
+                result = e.getKey();
+            }
+        }
+
+        return result;
     }
 
     public String breakVigenere(String pathToEncryptedFile, String pathToDictionary, char mostCommon) {
@@ -68,5 +111,22 @@ public class VigenereBreaker {
         String encrypted = fr.asString();
         HashSet<String> dictionary = readDictionary(pathToDictionary);
         return breakForLanguage(encrypted, dictionary, mostCommon);
+    }
+
+    public String breakForAllLangs(String encrypted) {
+        int max = 0;
+        String result = "";
+        String language = "";
+        for (Entry<String, HashSet<String>> e : languages.entrySet()) {
+            char c = mostCommonCharIn(e.getValue());
+            String decrypted = breakForLanguage(encrypted, e.getValue(), c);
+            int words = countWords(decrypted, e.getValue());
+            if (max < words) {
+                max = words;
+                result = decrypted;
+                language = e.getKey();
+            }
+        }
+        return result;
     }
 }
--- src/test/vigenere/VigenereBreakerTest.java
+++ src/test/vigenere/VigenereBreakerTest.java
@@ -3,6 +3,8 @@ package vigenere;
 import edu.duke.FileResource;
 import org.junit.Test;
 
+import java.util.HashSet;
+
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
@@ -37,7 +39,35 @@ public class VigenereBreakerTest {
     @Test
     public void testBreakVigenere() {
         VigenereBreaker vb = new VigenereBreaker();
-        String decrypted = vb.breakVigenere("resources/vigenere/athens_keyflute.txt", "resources/vigenere/dictionaries/English", 'e');
-        assertEquals("SCENE II", decrypted.substring(0, 8));
+        String decrypted = vb.breakVigenere("resources/vigenere/secretmessage2.txt", "resources/vigenere/dictionaries/English", 'e');
+        assertEquals("The Tragedy of Hamlet", decrypted.substring(0, 21));
+    }
+
+    @Test
+    public void testMostCommonCharIn() {
+        VigenereBreaker vb = new VigenereBreaker();
+        HashSet<String> dictionaryEng = vb.readDictionary("resources/vigenere/dictionaries/English");
+        char eng = vb.mostCommonCharIn(dictionaryEng);
+        HashSet<String> dictionaryDan = vb.readDictionary("resources/vigenere/dictionaries/Danish");
+        char dan = vb.mostCommonCharIn(dictionaryDan);
+        HashSet<String> dictionaryDut = vb.readDictionary("resources/vigenere/dictionaries/Dutch");
+        char dut = vb.mostCommonCharIn(dictionaryDut);
+        HashSet<String> dictionaryPor = vb.readDictionary("resources/vigenere/dictionaries/Portuguese");
+        char por = vb.mostCommonCharIn(dictionaryPor);
+        assertEquals('e', eng);
+        assertEquals('e', dan);
+        assertEquals('e', dut);
+        assertEquals('a', por);
+    }
+
+    @Test
+    public void testBreakForAllLangs() {
+        VigenereBreaker vb = new VigenereBreaker();
+        FileResource fr1 = new FileResource("resources/vigenere/secretmessage3.txt");
+        String actual1 = vb.breakForAllLangs(fr1.asString()).substring(0, 10);
+        FileResource fr2 = new FileResource("resources/vigenere/secretmessage4.txt");
+        String actual2 = vb.breakForAllLangs(fr2.asString()).substring(0, 21);
+        assertEquals("La chambre", actual1);
+        assertEquals("Drei Hexen treten auf", actual2);
     }
 }
