074cf77986a9404bce9287f00e32f04077453d39
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-17 11:37:03 +0100

    adding marker properties

--- src/main/earthquake/EarthQuakeMap.java
+++ src/main/earthquake/EarthQuakeMap.java
@@ -1,16 +1,16 @@
 package earthquake;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.OpenStreetMap;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
 
 public class EarthQuakeMap extends PApplet {
-
-    private int height = 600;
-    private int width = 800;
 
     private UnfoldingMap map;
 
@@ -20,16 +20,25 @@ public class EarthQuakeMap extends PApplet {
     }
 
     public void setup() {
-        Location kosciolLocation = new Location(50.06f, 19.938f);
-        SimplePointMarker kosciolMarker = new SimplePointMarker(kosciolLocation);
-        map = new UnfoldingMap(this, 0, 0, width, height, new OpenStreetMap.OpenStreetMapProvider());
-        map.zoomAndPanTo(12, new Location(50.06f, 19.938f));
+        size(800, 600);
+        map = new UnfoldingMap(this, 0, 0, 800, 600, new OpenStreetMap.OpenStreetMapProvider());
+        map.zoomAndPanTo(16, new Location(50.06f, 19.938f));
         MapUtils.createDefaultEventDispatcher(this, map);
-        map.addMarker(kosciolMarker);
-    }
 
-    public void settings() {
-        size(width, height);
+        //kosciol
+        Location kosciolLocation = new Location(50.0616f, 19.9393f);
+        Feature kosciolFeature = new PointFeature(kosciolLocation);
+        kosciolFeature.addProperty("year", "1347");
+        Marker kosciolMarker = new SimplePointMarker(kosciolLocation, kosciolFeature.getProperties());
+
+        //sukiennice
+        Location sukienniceLocation = new Location(50.0616f, 19.9373f);
+        Feature sukienniceFeature = new PointFeature(sukienniceLocation);
+        sukienniceFeature.addProperty("year", "1344");
+        Marker sukienniceMarker = new SimplePointMarker(sukienniceLocation, sukienniceFeature.getProperties());
+
+        
+        map.addMarkers(kosciolMarker, sukienniceMarker);
     }
 
     public void draw() {
