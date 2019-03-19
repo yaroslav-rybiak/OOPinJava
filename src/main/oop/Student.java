a0661280ca25f0d6b45b423b8ef3f87001b61146
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-19 18:29:33 +0100

    implementing overriding

--- src/main/oop/Student.java
+++ src/main/oop/Student.java
@@ -1,33 +1,45 @@
 package oop;
 
-/** A class to store information about a Student
- *  Used in module 4 of the UC San Diego MOOC Object Oriented Programming in Java
- * 
+/**
+ * A class to store information about a Student
+ * Used in module 4 of the UC San Diego MOOC Object Oriented Programming in Java
+ *
  * @author UC San Diego Intermediate Software Development MOOC team
- * 
  */
-public class Student extends Person
-{
-	public Student(String name)  {
-		super(name);
-	}
+public class Student extends Person {
+    private String id;
 
-	public boolean isAsleep( int hr ) // override 
-	{ 
-		return 2 < hr && 8 > hr; 
-	}
-
-	public String willDrink() {
-	    return "yes";
+    public Student(String name) {
+        super(name);
+        this.id = "unknown id";
     }
-	
-	public static void main(String[] args)
-	{
-		Person p = new Student("Sally");
-		((Student) p).willDrink();
 
-		Person s = new Person("Loh");
-		s = new Student("ne loh");
-        ((Student) s).willDrink();
-	}
+    public Student(String name, String id) {
+        super(name);
+        this.id = id;
+    }
+
+    public boolean isAsleep(int hr) // override
+    {
+        return 2 < hr && 8 > hr;
+    }
+
+    public String toString() {
+        return this.id + ": " + super.toString();
+    }
+
+    public String report() {
+        return "reporting";
+    }
+
+    public static void main(String[] args) {
+        Student p = new Student("Eric", "007");
+        Person q = new Person("Anna");
+        Person r = new Student("Rose");
+        Person s = new Student("John", "008");
+        System.out.println(p.toString());
+        System.out.println(q.toString());
+        System.out.println(r.toString());
+        System.out.println(s.toString());
+    }
 }
