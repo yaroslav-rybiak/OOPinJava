706dc58d046a6c82d510ce0e751bac68b9bebc8f
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-29 12:16:27 +0100

    more tests

--- src/test/algorythms/SortImplementationTest.java
+++ src/test/algorythms/SortImplementationTest.java
@@ -9,20 +9,35 @@ public class SortImplementationTest {
 
     @Test
     public void testCollectionSort() {
-        int[] assorted = new int[5];
-        assorted[0] = 8;
-        assorted[1] = 1;
-        assorted[2] = 29;
-        assorted[3] = 22;
-        assorted[4] = 15;
+        int[] assorted1 = new int[5];
+        assorted1[0] = 8;
+        assorted1[1] = 1;
+        assorted1[2] = 29;
+        assorted1[3] = 22;
+        assorted1[4] = 15;
 
-        int[] sorted = collectionSort(assorted);
+        int[] sorted1 = collectionSort(assorted1);
 
-        assertEquals(1, sorted[0]);
-        assertEquals(8, sorted[1]);
-        assertEquals(15, sorted[2]);
-        assertEquals(22, sorted[3]);
-        assertEquals(29, sorted[4]);
+        assertEquals(1, sorted1[0]);
+        assertEquals(8, sorted1[1]);
+        assertEquals(15, sorted1[2]);
+        assertEquals(22, sorted1[3]);
+        assertEquals(29, sorted1[4]);
 
+        int[] assorted2 = new int[2];
+        assorted2[0] = 8;
+        assorted2[1] = 1;
+
+        int[] sorted2 = collectionSort(assorted2);
+
+        assertEquals(1, sorted2[0]);
+        assertEquals(8, sorted2[1]);
+
+        int[] assorted3 = new int[1];
+        assorted3[0] = 1;
+
+        int[] sorted3 = collectionSort(assorted3);
+
+        assertEquals(1, sorted3[0]);
     }
 }
