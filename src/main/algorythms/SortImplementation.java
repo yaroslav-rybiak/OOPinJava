4208c772736a62d3b97cfe9ee2d84dd3b26eaf84
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-29 12:21:08 +0100

    updating algorythm

--- src/main/algorythms/SortImplementation.java
+++ src/main/algorythms/SortImplementation.java
@@ -4,19 +4,16 @@ public class SortImplementation {
 
     public static int[] collectionSort(int[] assorted) {
         for (int i = 0; i < assorted.length - 1; i++) {
-            int[] mins = findMin(assorted, i);
-            int min = mins[0];
-            int minIndex = mins[1];
+            int minIndex = findMinIndex(assorted, i);
             int temp = assorted[i];
-            assorted[i] = min;
+            assorted[i] = assorted[minIndex];
             assorted[minIndex] = temp;
 
         }
         return assorted;
     }
 
-    private static int[] findMin(int[] assorted, int start) {
-        int[] result = new int[2];
+    private static int findMinIndex(int[] assorted, int start) {
         int min = assorted[start];
         int minIndex = start;
         for (int i = start; i < assorted.length; i++) {
@@ -25,9 +22,6 @@ public class SortImplementation {
                 minIndex = i;
             }
         }
-        result[0] = min;
-        result[1] = minIndex;
-
-        return result;
+        return minIndex;
     }
 }
