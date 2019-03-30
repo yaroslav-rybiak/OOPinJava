34cda8a354c136461c013ba9f81aaac90c65d6ca
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2019-03-30 22:08:16 +0100

    adding swap method

--- src/main/algorythms/SortImplementation.java
+++ src/main/algorythms/SortImplementation.java
@@ -5,10 +5,7 @@ public class SortImplementation {
     public static int[] collectionSort(int[] assorted) {
         for (int i = 0; i < assorted.length - 1; i++) {
             int minIndex = findMinIndex(assorted, i);
-            int temp = assorted[i];
-            assorted[i] = assorted[minIndex];
-            assorted[minIndex] = temp;
-
+            assorted = swap(assorted, i, minIndex);
         }
         return assorted;
     }
@@ -23,5 +20,12 @@ public class SortImplementation {
             }
         }
         return minIndex;
+    }
+
+    private static int[] swap(int[] assorted, int a, int b) {
+        int temp = assorted[a];
+        assorted[a] = assorted[b];
+        assorted[b] = temp;
+        return assorted;
     }
 }
